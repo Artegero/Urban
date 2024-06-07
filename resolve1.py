97a2754c2fd6b62b21931853c5751016d0663ee0
Dmitriy Art <Dmitry-art95@ya.ru>
2024-06-07 12:04:54 +0700

    Add files via upload

---
+++ resolve1.py
@@ -0,0 +1,24 @@
+a='Hi people'
+print(len(a))
+
+first=4
+second=8
+summ=first+second
+diff=first-second
+print('Сумма:', summ, 'Разность:', diff)
+
+first=54
+second=32
+third=45
+mean=(first+second+third)/3
+print(mean)
+
+first_string='вторник'
+second_string='понедельник'
+print(second_string + ', ' + first_string)
+
+a=6
+b=4
+c=1
+f=(((a*b)+(a*c))**3)/2
+print(f)
