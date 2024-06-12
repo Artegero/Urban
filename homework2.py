0cd63a8269b77f25b6c0a635630ff7dd42422c53
Dmitriy Art <Dmitry-art95@ya.ru>
2024-06-12 23:19:18 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+completed_tasks=12
+time_spent=1.5
+name_kurs = 'Python'
+time_to_one_task = (time_spent/completed_tasks)
+print(name_kurs + ', всего задач:', str(completed_tasks) +
+      ', затрачено часов:', str(time_spent) +', среднее время выполнения:',
+      time_to_one_task, 'часа.')
